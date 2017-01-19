e387d31b311f7e17640b2191d16061ba0181d16d
Manish Wadhwani <manish.wadhwani@digital.hmrc.gov.uk>
2017-01-19 14:07:54 +0000

    updated

--- src/main/java/org/jsontocsv/Main.java
+++ src/main/java/org/jsontocsv/Main.java
@@ -20,42 +20,42 @@ import java.io.File;
 import java.net.URI;
 import java.util.List;
 import java.util.Map;
-
+import java.util.Set;
 import org.jsontocsv.parser.JSONFlattener;
 import org.jsontocsv.writer.CSVWriter;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        /*
+       /* *//*
          *  Parse a JSON String and convert it to CSV
-         */
+         *//*
         List<Map<String, String>> flatJson = JSONFlattener.parseJson(jsonString());
         // Using the default separator ','
-        CSVWriter.writeToFile(CSVWriter.getCSV(flatJson), "files/sample_string.csv");
+        CSVWriter.writeToFile(CSVWriter.getCSV(flatJson), "files/sample_string.csv");*/
 
         /*
          *  Parse a JSON File and convert it to CSV
          */
-        flatJson = JSONFlattener.parseJson(new File("files/simple.json"), "UTF-8");
+        List<Map<String, String>>  flatJson = JSONFlattener.parseJson(new File("files/gro/400100015.json"), "UTF-8");
         // Using ';' as separator
-        CSVWriter.writeToFile(CSVWriter.getCSV(flatJson, ";"), "files/sample_file.csv");
+        CSVWriter.writeToFile(CSVWriter.getCSV(flatJson, ";"), "files/gro/400100015_new.csv");
 
         /*
          *  Parse JSON from URL and convert it to CSV
-         */
+      /*   *//*
         flatJson = JSONFlattener.parseJson(new URI("http://echo.jsontest.com/firstname/Brahim/lastName/Arkni"));
         // Using '\t' as separator
         CSVWriter.writeToFile(CSVWriter.getCSV(flatJson, "\t"), "files/sample_uri.csv");
 	
-	/*
+	*//*
          *  Parse a Large JSON File and convert it to CSV
-         */
+         *//*
         flatJson = JSONFlattener.parseJson(new File("files/sample_large.json"), "UTF-8");
         // Using ';' as separator
         Set<String> header = CSVWriter.collectOrderedHeaders(flatJson);
         // the intention is generate a csv file with specific headers - not all
-        CSVWriter.writeLargeFile(flatJson, ";", "files/sample_largeFile.csv", header);  
+        CSVWriter.writeLargeFile(flatJson, ";", "files/sample_largeFile.csv", header); */
     }
 
     private static String jsonString() {
